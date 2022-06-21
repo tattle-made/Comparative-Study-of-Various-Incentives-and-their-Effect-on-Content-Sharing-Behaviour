54357c3d4a5547e3f53488e9deb97f3c89de4df6
dennyabrain <denny.george90@gmail.com>
2022-06-21 13:21:00 +0530

    test

--- actions/post-status-breakdown/index.js
+++ actions/post-status-breakdown/index.js
@@ -3,6 +3,7 @@ const { GoogleSpreadsheet } = require("google-spreadsheet");
 const doc = new GoogleSpreadsheet(
   "14RYZt4UofeRyascpsyjagnxYQ3kbgJMB9B5vayE5H9Y"
 );
+const mysql = require("mysql2/promise");
 
 const sleep = (time) =>
   new Promise((res) => setTimeout(res, time, "done sleeping"));
@@ -21,19 +22,37 @@ try {
       sqlPassword,
     });
 
-    await doc.useServiceAccountAuth(googleServiceAccountCredentials);
-    await doc.loadInfo();
-    const sheet = doc.sheetsByIndex[3];
-    const rows = await sheet.getRows();
+    const connection = mysql.createConnection({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: "psych_study_platform_production",
+    });
 
-    for (var i = 0; i < rows.length; i++) {
-      try {
-        console.log({ username: rows[i].username, password: rows[i].password });
-      } catch (err) {
-        console.log(`Error Saving row ${i}`);
-        console.log(err);
-      }
-    }
+    const [rows, fields] = await connection.execute(`
+      SELECT Count(Users.username), StudyPhases.stage 
+      FROM StudyPhases
+      LEFT JOIN Users
+      ON Users.id = StudyPhases.user
+      WHERE StudyPhases.current =TRUE 
+      GROUP BY StudyPhases.stage
+    `);
+
+    console.log(rows);
+
+    // await doc.useServiceAccountAuth(googleServiceAccountCredentials);
+    // await doc.loadInfo();
+    // const sheet = doc.sheetsByIndex[3];
+    // const sheetRows = await sheet.getRows();
+
+    // for (var i = 0; i < sheetRows.length; i++) {
+    //   try {
+    //     // console.log({ username: rows[i].username, password: rows[i].password });
+    //   } catch (err) {
+    //     console.log(`Error Saving row ${i}`);
+    //     console.log(err);
+    //   }
+    // }
   })();
 } catch (err) {
   core.setFailed(err.message);
