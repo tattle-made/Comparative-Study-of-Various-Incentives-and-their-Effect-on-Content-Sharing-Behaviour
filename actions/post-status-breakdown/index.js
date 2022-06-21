17e52799320958e8b85219a674e0aa24c5c5c5e7
dennyabrain <denny.george90@gmail.com>
2022-06-21 13:05:30 +0530

    test

--- actions/post-status-breakdown/index.js
+++ actions/post-status-breakdown/index.js
@@ -8,31 +8,33 @@ const sleep = (time) =>
   new Promise((res) => setTimeout(res, time, "done sleeping"));
 
 try {
-  console.log("Hello");
-  const googleServiceAccountCredentials = JSON.parse(
-    process.env.GOOGLE_CREDENTIALS
-  );
-  const sqlUsername = process.env.DB_USERNAME;
-  const sqlPassword = process.env.DB_PASSWORD;
-  console.log({
-    projectId: githubCredentials.project_id,
-    sqlUsername,
-    sqlPassword,
-  });
+  (async function () {
+    console.log("Hello");
+    const googleServiceAccountCredentials = JSON.parse(
+      process.env.GOOGLE_CREDENTIALS
+    );
+    const sqlUsername = process.env.DB_USERNAME;
+    const sqlPassword = process.env.DB_PASSWORD;
+    console.log({
+      projectId: githubCredentials.project_id,
+      sqlUsername,
+      sqlPassword,
+    });
 
-  await doc.useServiceAccountAuth(googleServiceAccountCredentials);
-  await doc.loadInfo();
-  const sheet = doc.sheetsByIndex[3];
-  const rows = await sheet.getRows();
+    await doc.useServiceAccountAuth(googleServiceAccountCredentials);
+    await doc.loadInfo();
+    const sheet = doc.sheetsByIndex[3];
+    const rows = await sheet.getRows();
 
-  for (var i = 0; i < rows.length; i++) {
-    try {
-      console.log({ username: rows[i].username, password: rows[i].password });
-    } catch (err) {
-      console.log(`Error Saving row ${i}`);
-      console.log(err);
+    for (var i = 0; i < rows.length; i++) {
+      try {
+        console.log({ username: rows[i].username, password: rows[i].password });
+      } catch (err) {
+        console.log(`Error Saving row ${i}`);
+        console.log(err);
+      }
     }
-  }
+  })();
 } catch (err) {
   core.setFailed(err.message);
 }
