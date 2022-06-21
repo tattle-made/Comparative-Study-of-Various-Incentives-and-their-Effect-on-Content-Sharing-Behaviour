bc4fa4e5d0903b90b3977ea865959887550382ad
dennyabrain <denny.george90@gmail.com>
2022-06-21 12:53:56 +0530

    check secrets in action

--- actions/post-status-breakdown/index.js
+++ actions/post-status-breakdown/index.js
@@ -3,7 +3,18 @@ const github = require("@actions/github");
 
 try {
   console.log("Hello");
-  core.setOutput("hello", { user: "tattle" });
+  const githubCredentials = process.env.GOOGLE_CREDENTIALS;
+  const sqlUsername = process.env.DB_USERNAME;
+  const sqlPassword = process.env.DB_PASSWORD;
+  console.log({
+    projectId: githubCredentials.project_id,
+    sqlUsername,
+    sqlPassword,
+  });
+  // core.setOutput("hello", { user: "tattle" });
 } catch (err) {
   core.setFailed(err.message);
 }
+
+// console.log(github.secrets.GITHUB_CREDENTIALS)
+// sql credentials
