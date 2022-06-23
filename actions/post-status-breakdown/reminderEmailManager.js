f53d30887ace8efc767fc90b29617af998844d42
dennyabrain <denny.george90@gmail.com>
2022-06-23 23:35:05 +0530

    test

---
+++ actions/post-status-breakdown/reminderEmailManager.js
@@ -0,0 +1,3 @@
+exports.scheduleReminderEmais = (user) => {};
+
+exports.sendReminderEmails = (user) => {};
---
+++ actions/post-status-breakdown/userStudyEmailManager.js
@@ -0,0 +1,9 @@
+const { GoogleSpreadsheet } = require("google-spreadsheet");
+const doc = new GoogleSpreadsheet(
+  "14RYZt4UofeRyascpsyjagnxYQ3kbgJMB9B5vayE5H9Y"
+);
+const mysql = require("mysql2/promise");
+
+exports.scheduleEmails = async (googleSheetUser) => {};
+
+exports.sendEmails = (googleSheetUser) => {};
